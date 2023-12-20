22407c5a6587b009a431c102676f184550725656
saranedubridge <saranraj.1803164@srec.ac.in>
2023-12-20 10:28:47 +0530

    urlshortner

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Style/Style.css'; // Import the CSS file with your styles
 
-function Dashboard() {
-  const navigate = useNavigate();
+function UrlDashboard() {
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+  const [error, setError] = useState('');
 
-  const handleLogout = () => {
-    navigate('/login');
+  const GenerateShortUrl = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/url', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ longUrl }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setShortUrl(`http://localhost:3001/${data.shortUrl}`); 
+      } else {
+        const errorData = await response.json();
+        setError(errorData.error || 'Failed to generate short URL');
+      }
+    } catch (error) {
+      console.error('Error generating short URL:', error);
+      setError('Error generating short URL');
+    }
   };
 
   return (
-    <div className="register-container"> {/* Apply the same container class */}
-      <div className="register-form"> {/* Apply the same form class */}
-        <h2 className="text-center">Welcome!</h2>
-        <button onClick={handleLogout} className="btn btn-primary">
-          Logout
-        </button>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="card-title">Create a Short URL</h2>
+              <input
+                type="text"
+                className="form-control mb-3"
+                placeholder="Enter a long URL"
+                value={longUrl}
+                onChange={(e) => setLongUrl(e.target.value)}
+              />
+              <button className="btn btn-primary" onClick={GenerateShortUrl}>
+                Generate Short URL
+              </button>
+              {shortUrl && (
+                <div className="mt-3">
+                  <p className="card-text">Short URL:</p>
+                  <a href={shortUrl} target="_blank" rel="noopener noreferrer" className="card-link">
+                    {shortUrl}
+                  </a>
+                </div>
+              )}
+              {error && (
+                <div className="alert alert-danger mt-3" role="alert">
+                  {error}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Dashboard;
-
+export default UrlDashboard;
